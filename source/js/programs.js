d6e50bdde2f0df7c392575ead5bf92ab7a88f3d1
DudakXen <dudak.xen@yandex.ru>
2025-02-06 21:12:14 +0300

    news block

--- source/js/programs.js
+++ source/js/programs.js
@@ -1,32 +1,29 @@
 // import Swiper JS
 import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
-import { Scrollbar } from 'swiper/modules';
-
+import { Scrollbar, Navigation } from 'swiper/modules';
 
 // import Swiper and modules styles
 import 'swiper/css';
-import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
+import 'swiper/css/navigation';
 
 
 const swiper = new Swiper('.programs__swiper', {
-  modules: [Navigation],
-  modules: [Scrollbar],
+  modules: [Navigation, Scrollbar],
 
   direction: 'horizontal',
   a11y: {
     prevSlideMessage: 'Предыдущий слайд',
     nextSlideMessage: 'Следующий слайд',
   },
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    draggable: true,
-  },
   // Navigation arrows
   navigation: {
     nextEl: '.programs__slider-button--next',
     prevEl: '.programs__slider-button--prev',
+  },
+  scrollbar: {
+    el: '.programs__scrollbar',
+    draggable: true,
   },
 
   loop: false,
@@ -37,7 +34,7 @@ const swiper = new Swiper('.programs__swiper', {
   initialSlide: 0,
   spaceBetween: 30,
 
-  _breakpoints: {
+  breakpoints: {
     320: {
       slidesPerView: 1,
       grabCursor: true,
@@ -54,12 +51,6 @@ const swiper = new Swiper('.programs__swiper', {
       simulateTouch: false,
       spaceBetween: 32,
     }
-  },
-  get breakpoints() {
-    return this._breakpoints;
-  },
-  set breakpoints(value) {
-    this._breakpoints = value;
-  },
+  }
 });
 
